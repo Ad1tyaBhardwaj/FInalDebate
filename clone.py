d810fd03577958d48dd536bacb9fc2c72c332176
Aditya Bhardwaj <adityabhardwaj98144@gmail.com>
2024-01-01 22:31:07 +0530

    Errors Removed - Files up to date

--- clone.py
+++ clone.py
@@ -1,7 +1,59 @@
 import streamlit as st
 import openai
+import time
+import os
+import gtts
+from dotenv import load_dotenv, find_dotenv
+import streamlit as st
+import pyttsx3
+load_dotenv('.env', override=True)
+text_speech = pyttsx3.init()
 
-st.title("ChatGPT-like clone")
+
+from langchain.chat_models import ChatOpenAI
+from langchain.schema import SystemMessage
+from langchain.chains import LLMChain
+from langchain.prompts import ChatPromptTemplate, HumanMessagePromptTemplate
+
+
+def debate_bot_response(content, topic, difficulty):
+    llm = ChatOpenAI(
+    model='gpt-3.5-turbo',
+    temperature=1,
+    openai_api_key='YOUR-OpenAI-APIKEY')
+
+    prompt = ChatPromptTemplate(
+    input_variables=['content'],
+    messages=[
+        SystemMessage(content=f'You are a debating with the person on the topic: "{topic}". You need to counter the points given by user. Assuming it as a game, you need to counter the user based on the difficulty given. On a scale of 1-10, where 1 means Giving very low level of counters and letting the user win, and 10 means aggressivly countering every point of user, and trying to defeat the user. The current difficulty is: "{difficulty}". Give responses in not more than 40 words.'),
+        HumanMessagePromptTemplate.from_template('{content}')
+       ]
+    )
+
+    chain = LLMChain(
+    llm=llm,
+    prompt=prompt,
+    verbose=False
+    )
+    if content in ['quit','exit','bye']:
+        print('Goodbye!')
+        return
+    response = chain.run({'content': content})
+    print(response)
+    return response
+
+
+debate_bot_response("I think AI will take away all jobs","future of AI",1)
+
+
+
+difficulty_global = 10
+topic_global = ""
+
+
+
+#WEBSITE
+st.title("Debate Master")
 
 # Set OpenAI API key from Streamlit secrets
 
@@ -48,6 +100,11 @@ if prompt := st.chat_input("What is up?"):
     with st.chat_message("user"):
         st.markdown(prompt)
     # Display assistant response in chat message container
-    with st.chat_message("assistant"):
-        message_placeholder = st.empty()
-        full_response = ""
+
+    with st.spinner("Thinking..."):
+        response = debate_bot_response(prompt, topic_global, difficulty_global)
+        responsee = f"{response}"
+        with st.chat_message("assistant"):
+            st.markdown(responsee)
+    # Add assistant response to chat history
+    st.session_state.messages.append({"role": "assistant", "content": responsee})
